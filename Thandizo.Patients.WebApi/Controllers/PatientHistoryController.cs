485f5ff724c60350924ecbd021e67c695ab786cf
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-03-29 11:31:30 +0200

    added PatientHistoryController

---
+++ Thandizo.Patients.WebApi/Controllers/PatientHistoryController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Thandizo.ApiExtensions.Filters;
+using Thandizo.ApiExtensions.General;
+using Thandizo.DataModels.Patients;
+using Thandizo.Patients.BLL.Services;
+
+namespace Thandizo.Patients.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientHistoryController : ControllerBase
+    {
+        IPatientHistoryService _service;
+
+        public PatientHistoryController(IPatientHistoryService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("GetById")]
+        [CatchException(MessageHelper.GetItemError)]
+        public async Task<IActionResult> GetById([FromQuery] long historyId)
+        {
+            var response = await _service.Get(historyId);
+
+            if (response.IsErrorOccured)
+            {
+                return BadRequest(response.Message);
+            }
+
+            return Ok(response.Result);
+        }
+
+        [HttpGet("GetByPatient")]
+        [CatchException(MessageHelper.GetListError)]
+        public async Task<IActionResult> GetByPatient([FromQuery]long patientId)
+        {
+            var response = await _service.GetByPatient(patientId);
+
+            if (response.IsErrorOccured)
+            {
+                return BadRequest(response.Message);
+            }
+
+            return Ok(response.Result);
+        }
+
+        [HttpPost("Add")]
+        [CatchException(MessageHelper.AddNewError)]
+        public async Task<IActionResult> Add([FromBody]PatientHistoryDTO patientHistory)
+        {
+            var outputHandler = await _service.Add(patientHistory);
+            if (outputHandler.IsErrorOccured)
+            {
+                return BadRequest(outputHandler);
+            }
+
+            return Created("", outputHandler);
+        }
+    }
+}
