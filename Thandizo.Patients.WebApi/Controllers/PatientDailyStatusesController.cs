b192fb282d56002cd4ea444922f8ca6cf732b6ce
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-03-29 10:59:20 +0200

    added PatientDailyStatusesController

---
+++ Thandizo.Patients.WebApi/Controllers/PatientDailyStatusesController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Thandizo.ApiExtensions.Filters;
+using Thandizo.ApiExtensions.General;
+using Thandizo.DataModels.Patients;
+using Thandizo.Patients.BLL.Services;
+
+namespace Thandizo.Patients.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientDailyStatusesController : ControllerBase
+    {
+        IPatientDailyStatusService _service;
+
+        public PatientDailyStatusesController(IPatientDailyStatusService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("GetById")]
+        [CatchException(MessageHelper.GetItemError)]
+        public async Task<IActionResult> GetById([FromQuery] long submissionId)
+        {
+            var response = await _service.Get(submissionId);
+
+            if (response.IsErrorOccured)
+            {
+                return BadRequest(response.Message);
+            }
+
+            return Ok(response.Result);
+        }
+
+        [HttpGet("GetByPatient")]
+        [CatchException(MessageHelper.GetListError)]
+        public async Task<IActionResult> GetByPatient([FromQuery]long patientId)
+        {
+            var response = await _service.GetByPatient(patientId);
+
+            if (response.IsErrorOccured)
+            {
+                return BadRequest(response.Message);
+            }
+
+            return Ok(response.Result);
+        }
+
+        [HttpPost("Add")]
+        [CatchException(MessageHelper.AddNewError)]
+        public async Task<IActionResult> Add([FromBody]IEnumerable<PatientDailyStatusDTO> statuses)
+        {
+            var outputHandler = await _service.Add(statuses);
+            if (outputHandler.IsErrorOccured)
+            {
+                return BadRequest(outputHandler);
+            }
+
+            return Created("", outputHandler);
+        }
+    }
+}
