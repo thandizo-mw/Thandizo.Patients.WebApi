e2bfc669c7b8773ba1ad10b4afc10f34fbbe775d
Vitumbiko Vinkhumbo <vitumbiko.vinkhumbo@angledimension.com>
2020-03-31 12:40:58 +0200

    Added PatientLocationMovementsController class

---
+++ Thandizo.Patients.WebApi/Controllers/PatientLocationMovementsController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Thandizo.ApiExtensions.Filters;
+using Thandizo.ApiExtensions.General;
+using Thandizo.DataModels.Patients;
+using Thandizo.Patients.BLL.Services;
+
+namespace Thandizo.Patients.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientLocationMovementsController : ControllerBase
+    {
+        IPatientLocationMovementService _service;
+
+        public PatientLocationMovementsController(IPatientLocationMovementService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("GetById")]
+        [CatchException(MessageHelper.GetItemError)]
+        public async Task<IActionResult> GetById([FromQuery] long locationMovementId)
+        {
+            var response = await _service.Get(locationMovementId);
+
+            if (response.IsErrorOccured)
+            {
+                return BadRequest(response.Message);
+            }
+
+            return Ok(response.Result);
+        }
+
+        [HttpGet("GetByPatientId")]
+        [CatchException(MessageHelper.GetItemError)]
+        public async Task<IActionResult> GetByPatientId([FromQuery] long patientId)
+        {
+            var response = await _service.GetByPatient(patientId);
+
+            if (response.IsErrorOccured)
+            {
+                return BadRequest(response.Message);
+            }
+
+            return Ok(response.Result);
+        }
+
+        [HttpGet("GetAll")]
+        [CatchException(MessageHelper.GetListError)]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _service.Get();
+
+            if (response.IsErrorOccured)
+            {
+                return BadRequest(response.Message);
+            }
+
+            return Ok(response.Result);
+        }
+
+        [HttpPost("Add")]
+        [CatchException(MessageHelper.AddNewError)]
+        public async Task<IActionResult> Add([FromBody]PatientLocationMovementDTO patientLocationMovement)
+        {
+            var outputHandler = await _service.Add(patientLocationMovement);
+            if (outputHandler.IsErrorOccured)
+            {
+                return BadRequest(outputHandler);
+            }
+
+            return Created("", outputHandler);
+        }
+
+        [HttpPut("Update")]
+        [CatchException(MessageHelper.UpdateError)]
+        public async Task<IActionResult> Update([FromBody]PatientLocationMovementDTO patientLocationMovement)
+        {
+            var outputHandler = await _service.Update(patientLocationMovement);
+            if (outputHandler.IsErrorOccured)
+            {
+                return BadRequest(outputHandler);
+            }
+
+            return Ok(outputHandler);
+        }
+    }
+}
