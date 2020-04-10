a286c67a515cdcbb5902500430604fb0c7654abf
Henry Alfred Sauzande <henry.sauzande@angledimension.com>
2020-04-10 17:12:08 +0200

    Added template files for SMS and Email alerts

---
+++ Thandizo.Patients.WebApi/Models/MessageTemplateModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Thandizo.Patients.WebApi.Models
+{
+    public class MessageTemplateModel
+    {
+        public string EmailTemplateFilePath { get; set; }
+        public string SmsTemplateFilePath { get; set; }
+    }
+}
