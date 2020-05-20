ddb85942677dd8343dd89d90c7c6ef05ea0720d5
Vitumbiko Vinkhumbo <vitumbiko.vinkhumbo@angledimension.com>
2020-05-20 16:19:30 +0200

    Added GetByPatientByDate and GetPatientsByDate methods to IPatientDailyStatusesService and PatientDailyStatusesService classes

--- Thandizo.Patients.BLL/Services/PatientDailyStatusService.cs
+++ Thandizo.Patients.BLL/Services/PatientDailyStatusService.cs
@@ -43,6 +43,55 @@ namespace Thandizo.Patients.BLL.Services
             };
         }
 
+        public async Task<OutputResponse> GetPatientByDate(DateTime fromSubmittedDate, DateTime toSubmittedDate)
+        {
+            var patients = await _context.PatientDailyStatuses.Where(x => x.DateSubmitted >= fromSubmittedDate.AddHours(-2) && x.DateSubmitted < toSubmittedDate)
+                              .GroupBy(x => new
+                              {
+                                  x.PatientId,
+                                  x.Patient.FirstName,
+                                  x.Patient.OtherNames,
+                                  x.Patient.LastName,
+                                  x.Patient.IdentificationNumber
+                              }).Select(x => new PatientDTO
+                              {
+                                  PatientId = x.Key.PatientId,
+                                  FirstName = x.Key.FirstName,
+                                  OtherNames = x.Key.OtherNames,
+                                  LastName = x.Key.LastName,
+                                  IdentificationNumber = x.Key.IdentificationNumber
+                              }).ToListAsync();
+                       
+            return new OutputResponse
+            {
+                IsErrorOccured = false,
+                Result = patients
+            };
+        }
+
+        public async Task<OutputResponse> GetByPatientByDate(long patientId, DateTime fromSubmittedDate, DateTime toSubmittedDate)
+        {
+            var dailyStatuses = await _context.PatientDailyStatuses.Where(x => x.DateSubmitted >= fromSubmittedDate.AddHours(-2) && x.DateSubmitted < toSubmittedDate).Where(x => x.PatientId.Equals(patientId))
+                                .OrderBy(x => x.SymptomId)
+                                .Select(x => new PatientDailyStatusResponse
+                                {
+                                    CreatedBy = x.CreatedBy,
+                                    SymptomName = x.Symptom.SymptomName,
+                                    DateCreated = x.DateCreated,
+                                    DateSubmitted = x.DateSubmitted,
+                                    PatientId = x.PatientId,
+                                    SubmissionId = x.SubmissionId,
+                                    SymptomId = x.SymptomId
+                                })
+                                .ToListAsync();
+
+            return new OutputResponse
+            {
+                IsErrorOccured = false,
+                Result = dailyStatuses
+            };
+        }
+
         public async Task<OutputResponse> GetByPatient(long patientId)
         {
             var dailyStatuses = await _context.PatientDailyStatuses.Where(x => x.PatientId.Equals(patientId))
@@ -70,7 +119,7 @@ namespace Thandizo.Patients.BLL.Services
 
         public async Task<OutputResponse> Add(IEnumerable<PatientDailyStatusDTO> statuses)
         {
-            var submissionDate = DateTime.Now.Date;
+            var submissionDate = DateTime.UtcNow;
             var symptomsToSubmit = statuses.Select(x => x.SymptomId);
             var patientId = statuses.FirstOrDefault().PatientId;
 
@@ -95,7 +144,7 @@ namespace Thandizo.Patients.BLL.Services
                 foreach (var status in statuses)
                 {
                     var mappedStatus = new AutoMapperHelper<PatientDailyStatusDTO, PatientDailyStatuses>().MapToObject(status);
-                    mappedStatus.DateCreated = DateTime.UtcNow.AddHours(2);
+                    mappedStatus.DateCreated = DateTime.UtcNow;
                     mappedStatus.DateSubmitted = submissionDate;
 
                     await _context.PatientDailyStatuses.AddAsync(mappedStatus);
